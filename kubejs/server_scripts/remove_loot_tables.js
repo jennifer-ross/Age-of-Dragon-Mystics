e00c3c5ac23573b6de18656cb8c6e68d01566599
jennifer23 <38167698+jennifer2318@users.noreply.github.com>
2024-07-26 23:48:14 +0300

    Update configs

---
+++ kubejs/server_scripts/remove_loot_tables.js
@@ -0,0 +1,8 @@
+const mc = (id) => `minecraft:${id}`;
+
+// LootJS.lootTables((event) => {
+//     event
+//         .getLootTable(mc('chests/desert_pyramid'))
+//         .firstPool()
+//         .removeItem(mc('bone'))
+// })
---
+++ kubejs/server_scripts/remove_recipes.js
@@ -0,0 +1,21 @@
+/*
+* ServerEvents.recipes(callback) is a function that accepts another function,
+* called the "callback", as a parameter. The callback gets run when the
+* server is working on recipes, and then we can make our own changes.
+* When the callback runs, it is also known as the event "firing".
+*/
+
+const mc = (id) => `minecraft:${id}`;
+
+// Listen for the "recipes" server event.
+ServerEvents.recipes(event => {
+    // You can replace `event` with any name you like, as
+    // long as you change it inside the callback too!
+
+    // This part, inside the curly braces, is the callback.
+    // You can modify as many recipes as you like in here,
+    // without needing to use ServerEvents.recipes() again.
+
+    // Remove all recipes where output:
+    // event.remove({ output: mc('stone_pickaxe') })
+})
