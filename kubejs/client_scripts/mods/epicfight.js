f22247efec59e51667cefea9e4164f2a8d9b6b04
jennifer23 <38167698+jennifer2318@users.noreply.github.com>
2025-03-25 10:29:59 +0300

    Update epicfight.js

--- kubejs/client_scripts/mods/epicfight.js
+++ kubejs/client_scripts/mods/epicfight.js
@@ -18,20 +18,20 @@ const DistanceModifier = 1
  * @returns {LookType.Entity | LookType.Block}
  */
 function PlayerLookAt(player) {
-    const renderDistance = Client.options.renderDistance().get()
-    const trace = player.rayTrace(renderDistance + DistanceModifier, false)
+    const renderDistance = Client.options.renderDistance().get();
+    const trace = player.rayTrace(renderDistance + DistanceModifier, false);
 
-    if (trace.entity) {
-        return LookType.Entity
+    if (trace.entity && trace.entity.living) {
+        return LookType.Entity;
     }
 
     if (trace.block) {
-        return LookType.Block
+        return LookType.Block;
     }
 }
 
 ClientEvents.tick(event => {
-    let player = Client.player
+    let player = Client.player;
 
     if (!player) {
         return;
@@ -40,21 +40,21 @@ ClientEvents.tick(event => {
     if (player.age < 20) return;
 
     let lookType = PlayerLookAt(player)
-    let $ClientEngine = Java.loadClass('yesman.epicfight.client.ClientEngine').getInstance()
-    let playerPatch =  $ClientEngine.controllEngine.getPlayerPatch()
+    let $ClientEngine = Java.loadClass('yesman.epicfight.client.ClientEngine').getInstance();
+    let playerPatch =  $ClientEngine.controllEngine.getPlayerPatch();
 
     if (!playerPatch) {
         return;
     }
 
     let handItem = player.getHandSlots();
-    let rightHand = handItem[0]
-    let leftHand = handItem[1]
+    let rightHand = handItem[0];
+    let leftHand = handItem[1];
 
-    let isRightHandDamageable = rightHand.damageableItem
-    let isLeftHandDamageable = leftHand.damageableItem
-    let isPickaxe = rightHand.displayName.toString().includes("_pickaxe")
-    let isAir = rightHand.displayName.toString().includes("air")
+    let isRightHandDamageable = rightHand.damageableItem;
+    let isLeftHandDamageable = leftHand.damageableItem;
+    let isPickaxe = rightHand.displayName.toString().includes("_pickaxe");
+    let isAir = rightHand.displayName.toString().includes("air");
 
     if (lookType === LookType.Entity && (isRightHandDamageable || isLeftHandDamageable || isAir) && !isPickaxe && !playerPatch.isChargingSkill() && !playerPatch.isBattleMode()) {
         playerPatch.toMode(PlayerMode.Battle, true);
