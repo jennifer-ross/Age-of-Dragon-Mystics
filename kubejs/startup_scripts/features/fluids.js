a063995d266e76983af97344a9ee85fce88f7e30
jennifer23 <38167698+jennifer2318@users.noreply.github.com>
2025-07-22 02:29:43 +0300

    Fix cfg & Fluid registry

---
+++ kubejs/startup_scripts/features/fluids.js
@@ -0,0 +1,8 @@
+// priority: 900
+
+global.EventsHandler.addStartupEvent('StartupEvents.registry.fluid', /** @param {Registry.Fluid} e */ e => {
+    // e.create()
+    //     .thickTexture(0x000033)
+    //     .bucketColor(0x000033)
+    //     .displayName('Volatile Sky Solution')
+});
