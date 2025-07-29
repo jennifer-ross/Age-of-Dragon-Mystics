b596e9e93fa3df1ba8b94f8c917c96bd61169a84
jennifer23 <38167698+jennifer2318@users.noreply.github.com>
2025-07-30 00:18:10 +0300

    Update texture

--- kubejs/startup_scripts/features/items.js
+++ kubejs/startup_scripts/features/items.js
@@ -4,7 +4,7 @@ let ns = global.Namespace;
 
 global.EventsHandler.addStartupEvent('StartupEvents.registry.item', /** @param {Registry.Item} e */ e => {
     e.create('dark_dragon_blood_bottle')
-        .texture('kubejs:item/dark_dragon_blood')
+        .texture('kubejs:item/dark_dragon_blood_bottle')
         .rarity('epic')
         .glow(false)
 });
